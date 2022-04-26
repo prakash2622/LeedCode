dd35d2aeddc8cb35c38154ea3f6e53c46e22c21b
prakash2622 <99064361+prakash2622@users.noreply.github.com>
2022-04-26 17:59:57 +0530

    Create SearchinRotatedSortedArray.java

---
+++ SearchinRotatedSortedArray.java
@@ -0,0 +1,12 @@
+public int search(int[] nums, int target) {
+        
+        if(nums.length==0){
+            return -1;
+        }
+       else{
+           for(int i=0;i<nums.length;i++){
+             if(nums[i]==target)
+                 return i;
+           }
+           return -1;
+         }
